b9a5b924f01fea0fd215be7fae29e09b22cc9c02
haeyoon1 <hynam1220@naver.com>
2026-05-19 21:43:32 +0900

    feat: 스키마 업데이트

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS reservation;
 DROP TABLE IF EXISTS theme;
 DROP TABLE IF EXISTS reservation_time;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE reservation_time
 (
@@ -29,4 +30,12 @@ CREATE TABLE reservation
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
+);
+
+CREATE TABLE users
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
